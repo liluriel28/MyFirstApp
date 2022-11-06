109fdda56a0b22e4b635e7651306eff1b72f5f83
liluriel28 <a3521110073@alumno.uttehuacan.edu.mx>
2022-11-06 17:43:10 -0600

    primera subida

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
+  constructor(private alertController: AlertController) {}
 
-  constructor() {}
+  async ReiniciarTap() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Has alcanzado el número maximo de clicks',
+      message: 'Se reiniciara el contador',
+      buttons: ['OK'],
+    });
 
+    //console.log("Reiniciado");
+    // this.counter=0;
+    await alert.present();
+  }
+
+  //Empieza el contador
+  counter: number = 0;
+  // databinding
+  TapCounter(){
+    console.log("Clicks");
+    this.counter++;
+  }
+  
+  LimpiarData(counter){
+    if(this.counter>10){
+      this.ReiniciarTap();
+      this.counter=0;
+      
+    }
+  }
+  // ReiniciarTap(){
+  //   console.log("Reiniciado");
+  //   this.counter=0;
+  // }
 }
